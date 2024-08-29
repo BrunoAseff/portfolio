25d780237750bce054c9cc2d1c70b4bb7c1e1a48
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-08-29 16:28:05 +0000

    adds blend mode

--- components/general/BottomLeftText.jsx
+++ components/general/BottomLeftText.jsx
@@ -2,15 +2,16 @@ export default function BottomLeftText() {
   return (
     <main className="w-full left-0 bottom-0 absolute">
       <div className="relative left-0 bottom-0">
-        <div className="w-[200px] text-gray-500 items-center flex justify-center text-center text-sm  rounded-tr-2xl bg-white  h-[60px]">
-          Textinho bem menor aqui
+        <div class="relative bg-[radial-gradient(circle_at_top_right,_transparent_30px,_#000_30px)] h-[50px] w-[100px] right-0"></div>
+
+        <div className="w-[200px] text-gray-500 items-center flex justify-center text-center text-sm  rounded-tr-3xl bg-white  h-[60px]">
+          <p> Textinho bem menor aqui</p>
         </div>
-        <div className="w-[400px] items-center flex justify-center text-center text-2xl font-bold rounded-tr-2xl bg-white  h-[80px]">
-          Algum texto escrito aqui
+        <div className="w-[400px] mix-blend-screen items-center flex justify-center text-center text-3xl font-bold rounded-tr-3xl bg-white  h-[80px]">
+          <h1> FRONTEND DEVELOPER</h1>
         </div>
-        <div className="w-[600px] bg-white flex items-center justify-center text-center text-2xl  rounded-tr-2xl  h-[80px]">
-          {" "}
-          Algum texto escrito um pouco maior
+        <div className=" bg-white  w-[500px] flex items-center justify-center text-center   rounded-tr-3xl  h-[80px]">
+          <h1 className="text-4xl   font-bold	 "> Bruno de Almeida Aseff</h1>
         </div>
       </div>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
   ],
   prefix: "",
   theme: {
+    fontFamily: {
+      handjet: ["var(--font-handjet)"],
+    },
     container: {
       center: true,
       padding: "2rem",
