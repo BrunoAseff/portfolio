535f61ddd3fb8eb437176d91e13250e8c88690e9
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-09-09 13:06:42 +0000

    corrects hydration errors and adds svg

--- components/general/BottomNav.jsx
+++ components/general/BottomNav.jsx
@@ -15,15 +15,15 @@ export default function BottomNav({ tabsState, onTabClick }) {
       <TooltipProvider delayDuration="50">
         <Tooltip>
           <TooltipTrigger>
-            <button
+            <div
               onClick={() => onTabClick("aboutMe")}
               className="p-3 text-md flex flex-col items-center justify-center gap-1 text-black cursor-pointer"
             >
               <User size={32} />
               {tabsState.aboutMe !== "closed" && (
-                <div className="rounded-full w-1 h-1 bg-black"></div>
+                <span className="rounded-full w-1 h-1 bg-black"></span>
               )}
-            </button>
+            </div>
           </TooltipTrigger>
           <TooltipContent className="bg-white/20 mb-1 shadow-xl backdrop-blur-[30px] border border-white/40">
             <p>Sobre mim</p>
@@ -34,15 +34,15 @@ export default function BottomNav({ tabsState, onTabClick }) {
       <TooltipProvider delayDuration="50">
         <Tooltip>
           <TooltipTrigger>
-            <button
+            <div
               onClick={() => onTabClick("technologies")}
               className="p-3 text-md flex flex-col items-center justify-center gap-1 text-black cursor-pointer"
             >
               <Code size={32} />
               {tabsState.technologies !== "closed" && (
-                <div className="rounded-full w-1 h-1 bg-black"></div>
+                <span className="rounded-full w-1 h-1 bg-black"></span>
               )}
-            </button>
+            </div>
           </TooltipTrigger>
           <TooltipContent className="bg-white/20 mb-1 shadow-xl backdrop-blur-[30px] border border-white/40">
             <p>Habilidades</p>
