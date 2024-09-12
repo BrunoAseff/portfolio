938054a46f9d8560cef4c8d47c22735f5e2c11fa
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2024-09-12 16:29:13 +0000

    adds contact page

--- app/contato/page.js
+++ app/contato/page.js
@@ -9,8 +9,11 @@ import {
   GithubLogo,
   LinkedinLogo,
   WhatsappLogo,
+  DownloadSimple,
+  ArrowSquareOut,
 } from "phosphor-react";
 import { toast } from "sonner";
+import Image from "next/image";
 
 export default function Contato() {
   const [copiedItem, setCopiedItem] = useState(null);
@@ -25,41 +28,63 @@ export default function Contato() {
 
   return (
     <main className="flex w-full overflow-hidden items-center justify-center h-screen">
-      <Card className="backdrop-blur-[20px] p-10 flex-col mt-auto mb-auto flex rounded-xl bg-white/20 border border-white/40">
+      <Card className="backdrop-blur-[20px] p-6 flex-col mt-auto mb-auto flex rounded-xl bg-white/20 border border-white/40">
         <CardHeader className="flex gap-1 mb-4 text-lg rounded-lg pl-0 p-2 w-fit font-semibold">
           <h1 className="mt-auto mb-4 font-semibold text-black">Contato</h1>
         </CardHeader>
-        <CardContent className="flex flex-col gap-4 items-center justify-center">
-          <div className="flex gap-4">
-            <LinkedinLogo size={25} />
-            <GithubLogo size={25} />
+        <CardContent className="flex flex-col  gap-10 items-center justify-center">
+          <div className="flex flex-col w-full justify-start gap-2">
+            <Button className="flex items-center justify-between backdrop-blur-[20px] gap-4 hover:bg-white/5 rounded-xl bg-white/10 border border-white/40">
+              <LinkedinLogo color="black" weight="fill" size={25} />
+              <strong className="text-black">Linkedin</strong>
+
+              <ArrowSquareOut color="black" size={23} />
+            </Button>
+            <Button className="flex items-center justify-between backdrop-blur-[20px] gap-4 hover:bg-white/5 rounded-xl bg-white/10 border border-white/40">
+              <Image
+                src="/svgs/github-light.svg"
+                alt="github logo"
+                width={25}
+                height={25}
+              />
+              <strong className="text-black">GitHub</strong>
+
+              <ArrowSquareOut color="black" size={23} />
+            </Button>
+
+            <Button
+              className="flex items-center justify-between backdrop-blur-[20px] gap-4 hover:bg-white/5 rounded-xl bg-white/10 border border-white/40"
+              onClick={() => copyToClipboard("(47) 991031009", "phone")}
+            >
+              <strong className="text-black">(47) 991031009</strong>
+              {copiedItem === "phone" ? (
+                <Check color="black" size={23} />
+              ) : (
+                <Copy color="black" size={23} />
+              )}
+            </Button>
+            <Button
+              className="flex items-center justify-between backdrop-blur-[20px] hover:bg-white/5 gap-4 rounded-xl bg-white/10 border border-white/40"
+              onClick={() => copyToClipboard("brunoaseff2@gmail.com", "email")}
+            >
+              <strong className="text-black">brunoaseff2@gmail.com</strong>
+              {copiedItem === "email" ? (
+                <Check color="black" size={23} />
+              ) : (
+                <Copy color="black" size={23} />
+              )}
+            </Button>
           </div>
-          <Button className="flex items-center gap-4">
-            <WhatsappLogo size={25} />
-            <p> Chamar no Whatsapp</p>
-          </Button>
-          <Button
-            className="flex items-center backdrop-blur-[20px] gap-4 hover:bg-white/5 rounded-xl bg-white/10 border border-white/40"
-            onClick={() => copyToClipboard("(47) 991031009", "phone")}
-          >
-            <strong className="text-black">(47) 991031009</strong>
-            {copiedItem === "phone" ? (
-              <Check color="black" size={25} />
-            ) : (
-              <Copy color="black" size={25} />
-            )}
-          </Button>
-          <Button
-            className="flex items-center backdrop-blur-[20px] hover:bg-white/5 gap-4 rounded-xl bg-white/10 border border-white/40"
-            onClick={() => copyToClipboard("brunoaseff2@gmail.com", "email")}
-          >
-            <strong className="text-black">brunoaseff2@gmail.com</strong>
-            {copiedItem === "email" ? (
-              <Check color="black" size={25} />
-            ) : (
-              <Copy color="black" size={25} />
-            )}
-          </Button>
+          <div className="flex flex-col gap-2 items-center justify-center">
+            <Button className="flex justify-evenly items-center w-full gap-4">
+              <DownloadSimple size={25} />
+              <p>Baixar currículo em PDF</p>
+            </Button>
+            <Button className="flex justify-evenly m-4 mt-0 w-full items-center gap-4">
+              <WhatsappLogo size={25} />
+              <p> Chamar no Whatsapp</p>
+            </Button>
+          </div>
         </CardContent>
       </Card>
     </main>
