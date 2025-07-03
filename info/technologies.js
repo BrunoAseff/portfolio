b470a69e9f0ce27b368d10d924c94b3dad1273fc
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-07-03 15:13:05 +0000

    fix: update tech hover colors

--- info/technologies.js
+++ info/technologies.js
@@ -26,65 +26,65 @@ export const technologies = [
   },
     {
     title: "TypeScript",
-    color: "hover:text-blue-600",
+    color: "hover:text-blue-400",
     icon: <SiTypescript size="2rem" />,
   },
     {
     title: "AWS",
-    color: "hover:text-orange-600",
+    color: "hover:text-orange-400",
     icon: <FaAws size="2rem" />,
   },
   {
     title: "Linux",
-    color: "hover:text-amber-600",
+    color: "hover:text-amber-400",
     icon: <SiLinux size="2rem" />,
   },
   {
     title: "Docker",
-    color: "hover:text-blue-500",
+    color: "hover:text-blue-400",
     icon: <SiDocker size="2rem" />,
   },
    {
     title: "Postgres",
-    color: "hover:text-blue-700",
+    color: "hover:text-blue-400",
     icon: <SiPostgresql size="2rem" />,
   },
   { title: "Git", color: "hover:text-orange-600", icon: <SiGit size="2rem" /> },
   {
     title: "Node.js",
-    color: "hover:text-green-600",
+    color: "hover:text-green-400",
     icon: <SiNodedotjs size="2rem" />,
   },
   {
     title: "React.js",
-    color: "hover:text-cyan-600",
+    color: "hover:text-cyan-400",
     icon: <SiReact size="2rem" />,
   },
   {
     title: "Next.js",
-    color: "hover:text-slate-600",
+    color: "hover:text-white",
     icon: <SiNextdotjs size="2rem" />,
   },
 
   {
     title: "Tailwind CSS",
-    color: "hover:text-sky-600",
+    color: "hover:text-sky-400",
     icon: <SiTailwindcss size="2rem" />,
   },
   {
     title: "Motion",
-    color: "hover:text-slate-600",
+    color: "hover:text-white",
     icon: <SiFramer size="2rem" />,
   },
   {
     title: "Styled Components",
-    color: "hover:text-pink-600",
+    color: "hover:text-pink-400",
     icon: <SiStyledcomponents size="2rem" />,
   },
 
   {
     title: "Prisma ORM",
-    color: "hover:text-slate-600",
+    color: "hover:text-white",
     icon: <SiPrisma size="2rem" />,
   },
 ];
