3260374086d368257426f38089ae3168968de382
Bruno Aseff <155178870+BrunoAseff@users.noreply.github.com>
2025-07-03 15:13:45 +0000

    feat: update tabs

--- components/Tabs/AboutMeTab.jsx
+++ components/Tabs/AboutMeTab.jsx
@@ -1,11 +1,15 @@
+import { getExperienceDuration } from "@/lib/utils";
 import { Rnd } from "react-rnd";
 
 export default function AboutMeTab({ state, onClose, onMinimize }) {
   if (state === "minimized") return null;
 
+
+const experienceText = getExperienceDuration(new Date("2025-02-03"));
+
   return (
     <Rnd
-      className="flex-col min-w-20 max-w-[40rem] p-8 text-black relative shadow-xl bg-white/20 backdrop-blur-[20px] border border-white/40 rounded-xl"
+      className="flex-col min-w-20 max-w-[40rem] p-8 relative shadow-xl bg-black/5 backdrop-blur-[40px] border border-white/40 rounded-xl z-50"
       default={{
         x: -600,
         y: -200,
@@ -27,20 +31,24 @@ export default function AboutMeTab({ state, onClose, onMinimize }) {
         />
         <button className="rounded-full w-4 h-4 border-1 border-gray-900 bg-green-400 absolute top-4 left-16" />
       </div>
-      <div className="flex gap-1 mb-4 text-lg rounded-lg pl-0 p-2 w-fit font-semibold mt-6">
+      <div className="flex gap-1 mb-4 text-2xl  pl-0 p-2 w-fit mt-6 text-white/80  drop-shadow-2xl ">
         <p>Sobre mim</p>
       </div>
-      <div className="items-center justify-center text-justify w-full">
-        <p>
-          Sou programador e estudante de Sistemas para Internet na Univali.
-          <br />
-          Tenho afinidade tanto com o desenvolvimento back-end quanto front-end,
-          mas tenho mais conhecimento em front-end.
-          <br />
-          Tenho interesse genuíno por linguística e gosto de estudar as
-          diferentes formas de se comunicar, seja através de códigos ou
-          palavras.
+      <div className="items-center justify-center text-base text-justify w-full text-white/80 drop-shadow-3xl">
+    <p>
+      Sou desenvolvedor fullstack há <strong>{experienceText}</strong> e estudante de Sistemas para Internet na Univali.
+    </p>
+    <br/>
+      <p>
+      Tenho bastante experiência com front-end, mas também trabalho com back-end e arquitetura de aplicações.
+      </p>
+          <br/>
+
+    <p>
+      Nos últimos tempos, venho focando cada vez mais em Cloud. Entender como aplicações escalam, se comunicam e se mantêm resilientes na infraestrutura é o que mais me atrai hoje.
         </p>
+  <br />
+
       </div>
     </Rnd>
   );
--- components/general/Technologieslogo.jsx
+++ components/general/Technologieslogo.jsx
@@ -11,11 +11,11 @@ export default function TechnologiesLogo({ iconColor, title, icon }) {
       <TooltipProvider delayDuration="50">
         <Tooltip>
           <TooltipTrigger
-            className={`${iconColor} text-black backdrop-blur-[30px] cursor-default  p-2 rounded-xl`}
+            className={`${iconColor} text-white/75 backdrop-blur-[100px] border border-white/10 bg-black/5 cursor-default  p-3 rounded-xl`}
           >
             {icon}
           </TooltipTrigger>
-          <TooltipContent className="bg-white/80 shadow-xl backdrop-blur-[30px] border border-white/40">
+          <TooltipContent className="bg-white/20 font-medium text-white shadow-xl backdrop-blur-[30px] border border-white/40">
             <p>{title}</p>
           </TooltipContent>
         </Tooltip>
