e3a5b84deb4d8a0ea877b66d5ff69b7a6a6a86f2
Bruno Aseff <brunoaseff2@gmail.com>
2025-12-29 12:23:51 -0300

    refactor: remove unused TabsMobile component from Home page

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,18 +1,11 @@
 "use client";
 
 import AboutMePage from "@/components/AboutMe/AboutMePage";
-import TabsMobile from "@/components/Tabs/TabsMobile";
 
 export default function Home() {
   return (
     <div className="w-full h-full">
-      <div className="md:hidden">
-        <TabsMobile />
-      </div>
-      
-      <div className="hidden md:block">
         <AboutMePage />
-      </div>
     </div>
   );
 }
