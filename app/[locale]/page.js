ce1c18d0064240694d3f12fe5177ab8828c46dfc
Bruno Aseff <brunoaseff2@gmail.com>
2025-12-29 12:20:19 -0300

    refactor: simplify Home component by replacing tab management with AboutMePage

--- app/[locale]/page.js
+++ app/[locale]/page.js
@@ -1,66 +1,18 @@
 "use client";
 
-import { useState } from "react";
-import BottomNav from "@/components/general/BottomNav";
-import AboutMeTab from "@/components/Tabs/AboutMeTab";
-import TechnologiesTab from "@/components/Tabs/TechnologiesTab";
+import AboutMePage from "@/components/AboutMe/AboutMePage";
 import TabsMobile from "@/components/Tabs/TabsMobile";
 
 export default function Home() {
-  const [tabsState, setTabsState] = useState({
-    aboutMe: "open",
-    technologies: "open",
-  });
-
-  const handleTabStateChange = (tabName) => {
-    setTabsState((prevState) => {
-      if (prevState[tabName] === "open")
-        return { ...prevState, [tabName]: "minimized" };
-      if (prevState[tabName] === "minimized")
-        return { ...prevState, [tabName]: "open" };
-      return { ...prevState, [tabName]: "open" };
-    });
-  };
-
-  const closeTab = (tabName) => {
-    setTabsState((prevState) => ({ ...prevState, [tabName]: "closed" }));
-  };
-
-  const minimizeTab = (tabName) => {
-    setTabsState((prevState) => ({ ...prevState, [tabName]: "minimized" }));
-  };
-
-  const restoreTab = (tabName) => {
-    setTabsState((prevState) => ({ ...prevState, [tabName]: "open" }));
-  };
-
   return (
-    <div
-      className="md:overflow-visible overflow-hidden"
-    >
-      <TabsMobile className="hidden" />
-      <main className="border-6 md:flex hidden border-blue-600 h-screen flex-col max-w-full justify-center items-center relative">
-        <section>
-          {tabsState.aboutMe !== "closed" && (
-            <AboutMeTab
-              state={tabsState.aboutMe}
-              onClose={() => closeTab("aboutMe")}
-              onMinimize={() => minimizeTab("aboutMe")}
-              onRestore={() => restoreTab("aboutMe")}
-            />
-          )}
-          {tabsState.technologies !== "closed" && (
-            <TechnologiesTab
-              state={tabsState.technologies}
-              onClose={() => closeTab("technologies")}
-              onMinimize={() => minimizeTab("technologies")}
-              onRestore={() => restoreTab("technologies")}
-            />
-          )}
-        </section>
-
-        <BottomNav tabsState={tabsState} onTabClick={handleTabStateChange} />
-      </main>
+    <div className="w-full h-full">
+      <div className="md:hidden">
+        <TabsMobile />
+      </div>
+      
+      <div className="hidden md:block">
+        <AboutMePage />
+      </div>
     </div>
   );
 }
